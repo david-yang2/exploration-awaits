1f138a2c2b1845dc48d28d1e5901c0f60a057c2b
David <david.yang026@gmail.com>
2021-04-27 17:09:32 -0700

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "static_pages#root"
+
+  namespace :api, defaults: {format: :json} do
+
+    resources :carlistings, except: [:new, :edit] do
+      resources :reviews, only: [:show]
+      resources :trips, only: [:show]
+    end
+    resources :users do 
+      resource :reviews, only: [:create,:update]
+    end
+    resource :session, only: [:new, :create, :destroy]
+  end
+  
 end
