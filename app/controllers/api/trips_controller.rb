1d72fa2bb315256c26a46098a5e054f8eb0d73f5
David <david.yang026@gmail.com>
2021-04-30 11:51:37 -0700

    created index, show, and create actions

--- app/controllers/api/trips_controller.rb
+++ app/controllers/api/trips_controller.rb
@@ -1,2 +1,29 @@
 class Api::TripsController < ApplicationController
+
+    #GET    /api/trips
+    def index
+        @carlisting = Carlisting.find(params[:carlisting_id])
+        render json: @carlisting.trips
+    end 
+
+    #GET    /api/carlistings/:carlisting_id/trips/:id
+    def show
+        @trip = Trip.find(params[:id])
+    end
+
+    #POST   /api/trips
+    def create
+        @trip = Trip.new(trip_params)
+        if @trip.save
+            return
+        else
+            render json: @trip.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def trip_params
+        params.require(:trip).permit(:carlisting_id, :beg_long, :beg_lat, :end_long, :end_lat)
+    end
+            
 end
