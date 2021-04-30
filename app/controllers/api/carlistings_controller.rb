2aa6d3749350406697a9c2be7b4096ab3d9b4f3b
David <david.yang026@gmail.com>
2021-04-30 12:09:12 -0700

    created index, show, create, update and destroy actions

--- app/controllers/api/carlistings_controller.rb
+++ app/controllers/api/carlistings_controller.rb
@@ -1,2 +1,51 @@
 class Api::CarlistingsController < ApplicationController
+    #GET    /api/carlistings 
+    def index
+        @carlistings = Carlisting.all
+    end
+
+    #GET    /api/carlistings/:id
+    def show
+        @carlisting = Carlisting.find(params[:id])
+    end
+
+    #POST   /api/carlistings
+    def create
+        @carlisting = Carlisting.new(carlisting_params)
+
+        if @carlisting.save
+            return
+        else 
+            render json: @carlisting.errors.full_messages, status: 422
+        end
+    end
+
+    #PATCH  /api/carlistings/:id
+    def update
+        @carlisting = Carlisting.find(params[:id])
+
+        if @carlisting.update(carlisting_params)
+            return
+        else
+            render json: @carlisting.errors.full_messages, status: 422
+        end
+    end
+
+    #DELETE /api/carlistings/:id
+    def destroy 
+        @carlisting = Carlisting.find(params[:id])
+
+        if @carlisting.destroy
+            return
+        else
+            render json: @carlisting.errors.full_messages, status: 422
+        end
+    end
+
+    private
+    def carlisting_params
+        params.require(:carlisting).permit(:owner_id, :year, :make, :model, :price, :cartype,:drivetrain,
+                                        :transmission, :tripcapacity, :seats, :sleeps, :lockers, :rooftoptent
+                                        :shower, :kitchenset, :fridge, :longitude, :latitude)
+    end
 end
