0d49b312931fdfa21425da954e16269e6b3239be
David <david.yang026@gmail.com>
2021-04-30 11:04:32 -0700

    created index, show, create, update, destory actions

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,3 +1,45 @@
 class Api::BookingsController < ApplicationController
+    def index
+        @bookings = Booking.all
+    end
+
+    def show
+        @booking = Booking.find(params[:id])
+    end
+
+    def create
+        @booking = Booking.create(booking_params)
+        if @booking.save
+            return
+        else
+            render json: @booking.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @booking = Booking.find(params[:id])
+
+        if @booking.update(booking_params)
+            return
+        else 
+            render json: @booking.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @booking = Booking.find(params[:id])
+
+        if @booking.destroy
+            return
+        else
+            render json: @booking.errors.full_messages, status: 422
+        end
+    end
+
+
+    private
+    def booking_params
+        params.require(:booking).permit(:)
+    end
 
 end
