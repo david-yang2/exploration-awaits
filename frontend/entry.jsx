e65db254e924cc88a04eb54f7d0b32d1c9082d9e
David <david.yang026@gmail.com>
2021-05-04 16:04:25 -0700

    included state and store

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./components/root";
-// import configureStore from "./store/store";
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () =>{
     const root = document.getElementById("root");
-    // let preloadedState = undefined;
+    let preloadedState = undefined;
 
-    // if(window.currentUser) {
-    //     preloadedState = {
-    //         session: {
-    //             currentUser: window.currentUsers
-    //         }
-    //     };
-    // }
-    // const store = configureStore(preloadedState);
+    if(window.currentUser) {
+        preloadedState = {
+            session: {
+                currentUser: window.currentUsers
+            }
+        };
+    }
+    const store = configureStore(preloadedState);
 
-    // ReactDOM.render(<Root store={store} />, root);
-    ReactDOM.render(<Root />, root);
+    ReactDOM.render(<Root store={store} />, root);
+
 });
