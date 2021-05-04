12e4de319b8803060ce070d2f38d611a1dd5ba35
David <david.yang026@gmail.com>
2021-05-04 15:59:50 -0700

    initial commit

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,23 @@
+import {
+    RECEIVE_CURRENT_USER, 
+    LOG_OUT_CURRENT_USER } from "../actions/session_actions";
+
+const _nullSession = {
+    currentUser: null,
+};
+
+
+const SessionReducer = (state = _nullSession, action) => {
+    Object.freeze(state);
+    switch(action.type) {
+        case RECEIVE_CURRENT_USER:
+            return Object.assign({}, {currentUser: action.user});
+
+        case LOG_OUT_CURRENT_USER:
+            return _nullSession;
+        default:
+            return state;
+    }
+};
+
+export default SessionReducer;
