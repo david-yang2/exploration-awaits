643c2f7953cf5893d48becb9617b7040a17e8ba5
David <david.yang026@gmail.com>
2021-05-06 13:42:14 -0700

    included route to login component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,13 +3,14 @@ import { Route } from "react-router-dom";
 import Home from "./home/home";
 import NavBarContainer from "./nav/navbar_container";
 import SignupContainer from "./sessions/signup_container";
+import LoginContainer from "./login/login_container";
 
 const App = () => (
     <div>
         <Route path="/" component={NavBarContainer} />
         <Route exact path="/" component ={Home} />
         <Route path="/signup" component={SignupContainer} />
-
+        <Route path="/login" component={LoginContainer} />
     </div>
 )
 
