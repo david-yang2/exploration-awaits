61e97287249ba93abb3e92492b220de1e447b91a
David <david.yang026@gmail.com>
2021-05-09 17:26:53 -0700

    updated belongs to association

--- app/models/carlisting.rb
+++ app/models/carlisting.rb
@@ -6,6 +6,6 @@ class Carlisting < ApplicationRecord
     
     belongs_to :user,
         primary_key: :id,
-        foreign_key: :user_id,
+        foreign_key: :owner_id,
         class_name: :User
 end
