9d40ea0b83de7ec5c0b829942db083f3721e5136
David <david.yang026@gmail.com>
2021-05-09 17:04:23 -0700

    add belongs to association with user

--- app/models/carlisting.rb
+++ app/models/carlisting.rb
@@ -3,4 +3,9 @@ class Carlisting < ApplicationRecord
         primary_key: :id,
         foreign_key: :carlisting_id,
         class_name: :Trip
+    
+    belongs_to :user,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
 end
