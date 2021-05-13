02187773e04fe9b748bdc9e85cedc96d3a4bcce6
David <david.yang026@gmail.com>
2021-05-12 23:45:24 -0700

    thunk action creators

---
+++ frontend/actions/carlisting_actions.js
@@ -0,0 +1,20 @@
+import * as CarlistingApiUtil from "../util/carlistings_api_util"
+
+
+// POJOs
+
+export const RECEIVE_ALL_CARLISTINGS = "RECEIVE_ALL_CARLISTINGS";
+export const RECEIVE_CARLISTING = "RECEIVE_CARLISTING";
+export const CREATE_CARLISTING = "CREATE_CARLISTING";
+export const UPDATE_CARLISTING = "UPDATE_CARLISTING";
+export const DELLETE_CARLISTING = "DELLETE_CARLISTING";
+
+
+
+
+
+
+
+
+
+// THUNK Action Creators
