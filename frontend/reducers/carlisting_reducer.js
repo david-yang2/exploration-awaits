08a14d2dafad09bc69a00ce28c65926d5b829a24
David <david.yang026@gmail.com>
2021-05-13 21:12:39 -0700

    created CarlistingReducer

--- frontend/reducers/carlisting_reducer.js
+++ frontend/reducers/carlisting_reducer.js
@@ -4,3 +4,27 @@ import {
     DELLETE_CARLISTING 
 } from "../actions/carlisting_actions";
 
+
+const CarlistingReducer = (oldState = {}, action) =>{
+    Object.freeze(oldState);
+
+
+    let nextState = Object.assign({}, oldState);
+
+    switch (action.type) {
+        case RECEIVE_ALL_CARLISTINGS:
+            return action.carlistings;
+
+        case RECEIVE_CARLISTING:
+            nextState[action.carlisting.id] = action.carlisting;
+            return nextState;
+
+        case DELLETE_CARLISTING:
+            delete nextState[action.carlistingId];
+            return nextState;
+        default:
+            return oldState;
+    }
+}
+
+export default CarlistingReducer;
