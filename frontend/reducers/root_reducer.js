2c67016a443645fd1cbe6a0978b72aa688a1cc82
David <david.yang026@gmail.com>
2021-05-13 21:16:41 -0700

    included CarlistingReducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,9 @@
 import {combineReducers} from "redux";
 import SessionReducer from "./session_reducer"
+import CarlistingReducer from "./carlisting_reducer"
 
 const RootReudcer = combineReducers({
+    carlisting: CarlistingReducer,
     session: SessionReducer
 
 });
