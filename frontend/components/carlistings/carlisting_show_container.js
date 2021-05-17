df20f9451455cbcb34b87fb9d2106bcee637337b
David <david.yang026@gmail.com>
2021-05-17 13:00:14 -0700

    mapped state/dispatch to props

--- frontend/components/carlistings/carlisting_show_container.js
+++ frontend/components/carlistings/carlisting_show_container.js
@@ -1,5 +1,13 @@
 import {connect} from "react-redux";
-import CarlistingShowIndex from "./carlistings_index";
+import CarlistingShow from "./carlisting_show";
 import {fetchCarlisting} from "../../actions/carlisting_actions"
 
+const mapStateToProps = (state) => ({
+    carlistings: state.carlistings
+})
 
+const mapDispatchToProps = dispatch => ({
+    fetchCarlisting: id => dispatch(fetchCarlisting(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CarlistingShow)
