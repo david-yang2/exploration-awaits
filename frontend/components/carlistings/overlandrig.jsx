1226d94b1698696e1d1e0db6ea40cddbb985ca56
David <david.yang026@gmail.com>
2021-05-17 14:24:19 -0700

    link to specific rigs is functional

--- frontend/components/carlistings/overlandrig.jsx
+++ frontend/components/carlistings/overlandrig.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-
+import {Link} from 'react-router-dom';
 
 const OverlandRig = (props) => {
-    
+    debugger
     return (
         <div>
             <h2>{props.rig.year} {props.rig.make} {props.rig.model}</h2>
+            <h5>$ {props.rig.price} / day</h5>
+            <br />
+            <Link to={`/carlistings/${props.rig.id}`}> Check out this rig</Link>
         </div>
     )
 
