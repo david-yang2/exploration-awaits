02c3701741dafad92edb92241f2943ce12766505
David <david.yang026@gmail.com>
2021-05-17 12:56:29 -0700

    initial commit

---
+++ frontend/components/carlistings/carlisting_show.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class CarlistingShow extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    
+
+    render(){
+        return null
+    }
+}
+
+export default CarlistingShow
