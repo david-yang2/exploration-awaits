51b1942b4dc21a48f413245f5b999fa580d20093
David <david.yang026@gmail.com>
2021-05-17 17:07:47 -0700

    initial commit

---
+++ frontend/components/reviews/review_container.js
@@ -0,0 +1,14 @@
+import {connect} from "react-redux";
+import Review from "./review";
+
+
+const mapStateToProps = state => ({
+    
+})
+
+const mapDispatchToProps = dispatch => ({
+
+})
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(Review)
