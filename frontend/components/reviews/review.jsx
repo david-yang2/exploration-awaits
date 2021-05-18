36a2d424c7bd8646527c3f65fa275d92d8f11046
David <david.yang026@gmail.com>
2021-05-17 17:09:23 -0700

    initial commit

---
+++ frontend/components/reviews/review.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class Review extends React.Component{
+    constructor(props){
+        super(props)
+
+    }
+
+
+    render(){
+        return null
+    }
+}
+
+export default Review;
