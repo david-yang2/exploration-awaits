da4313e84c86e2f6ca279e4088c8501e77d09da1
David <david.yang026@gmail.com>
2021-05-19 13:53:17 -0700

    included users reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,11 +2,13 @@ import {combineReducers} from "redux";
 import SessionReducer from "./session_reducer"
 import CarlistingsReducer from "./carlistings_reducer"
 import ReviewsReducer from "./reviews_reducer"
+import UsersReducer from "./user_reducer"
 
 const RootReudcer = combineReducers({
     carlistings: CarlistingsReducer,
     session: SessionReducer,
-    reviews: ReviewsReducer
+    reviews: ReviewsReducer,
+    users: UsersReducer
 
 });
 
