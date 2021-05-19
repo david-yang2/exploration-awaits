59fa90c24d1226a055ab7698cc40071282a5da6e
David <david.yang026@gmail.com>
2021-05-19 12:41:25 -0700

    included Review component

--- frontend/components/carlistings/carlisting_show.jsx
+++ frontend/components/carlistings/carlisting_show.jsx
@@ -1,26 +1,48 @@
 import React from "react";
+import Review from "../reviews/review"
 
 class CarlistingShow extends React.Component{
     constructor(props){
         super(props)
-    }
+    }   
     
     componentDidMount(){
-        this.props.fetchCarlisting(this.props.match.params.id);
+        debugger
+
+        this.props.fetchCarlisting(this.props.match.params.id)
+            .then(() => this.props.fetchReviews(this.props.match.params.id))
+
     }
 
     render(){
-        const {carlisting} = this.props;
+        debugger
+        const {carlisting, reviews} = this.props;
         if (!carlisting) return null
-
-        return(
+        debugger
+        return (!reviews || reviews.length?
             <div>
                 <div>
                     <h1>{carlisting.year} {carlisting.make} {carlisting.model}</h1>
+                    <Review reviews={reviews}/>
                 </div>
             </div>
+        :
+            null
         )
-    }
+        }
 }
 
+
 export default CarlistingShow
+
+
+
+        // Promise.all([this.props.fetchCarlisting(this.props.match.params.id),
+        //                 this.props.fetchReviews(this.props.match.params.id)])
+        //                 .then(([carlisting, reviews]) => {
+        //                     this.setState({
+        //                         carlisting: carlisting,
+        //                         reviews: reviews
+        //                     })
+        //                 })
+
