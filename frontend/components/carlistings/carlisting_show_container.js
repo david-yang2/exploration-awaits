5ffbc75b0496cad499a71db4c89adfff8eb11fc6
David <david.yang026@gmail.com>
2021-05-19 12:42:06 -0700

    mapped stated/dispatch for reviews

--- frontend/components/carlistings/carlisting_show_container.js
+++ frontend/components/carlistings/carlisting_show_container.js
@@ -1,13 +1,22 @@
 import {connect} from "react-redux";
 import CarlistingShow from "./carlisting_show";
-import {fetchCarlisting} from "../../actions/carlistings_actions"
+import {fetchCarlisting} from "../../actions/carlistings_actions";
+import {fetchReviews} from "../../actions/reviews_actions"
 
-const mapStateToProps = (state, ownProps) => ({
-    carlisting: state.carlistings[ownProps.match.params.id]
-})
+const mapStateToProps = (state, ownProps) => {
+    debugger
+    return {
+    carlisting: state.carlistings[ownProps.match.params.id],
+    reviews: state.reviews
+}
+}
 
-const mapDispatchToProps = dispatch => ({
-    fetchCarlisting: id => dispatch(fetchCarlisting(id))
-})
+const mapDispatchToProps = dispatch => {
+    debugger
+    return{
+    fetchCarlisting: id => dispatch(fetchCarlisting(id)),
+    fetchReviews: id => dispatch(fetchReviews(id))
+    }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(CarlistingShow)
