8795a9ee56ce41f07f8daa476a5364668564570e
David <david.yang026@gmail.com>
2021-05-20 15:24:53 -0700

    incorporated Review component

--- frontend/components/carlistings/carlisting_show.jsx
+++ frontend/components/carlistings/carlisting_show.jsx
@@ -7,23 +7,27 @@ class CarlistingShow extends React.Component{
     }   
     
     componentDidMount(){
-        debugger
 
         this.props.fetchCarlisting(this.props.match.params.id)
-            .then(() => this.props.fetchReviews(this.props.match.params.id))
+        this.props.fetchReviews(this.props.match.params.id)
+        this.props.fetchAllUsers()
+        // this.props.session.currentUser ? 
+        //     (this.props.fetchCurrentUser(this.props.session.currentUser.id))
+        //     : (null)
 
     }
 
     render(){
+        const {carlisting, reviews, user} = this.props;
         debugger
-        const {carlisting, reviews} = this.props;
         if (!carlisting) return null
         debugger
-        return (!reviews || reviews.length?
+        return (reviews.length != undefined && Object.keys(user).length >0 ?
             <div>
                 <div>
                     <h1>{carlisting.year} {carlisting.make} {carlisting.model}</h1>
-                    <Review reviews={reviews}/>
+                    <Review reviews={reviews}
+                            users={user}/>
                 </div>
             </div>
         :
@@ -35,14 +39,4 @@ class CarlistingShow extends React.Component{
 
 export default CarlistingShow
 
-
-
-        // Promise.all([this.props.fetchCarlisting(this.props.match.params.id),
-        //                 this.props.fetchReviews(this.props.match.params.id)])
-        //                 .then(([carlisting, reviews]) => {
-        //                     this.setState({
-        //                         carlisting: carlisting,
-        //                         reviews: reviews
-        //                     })
-        //                 })
 
