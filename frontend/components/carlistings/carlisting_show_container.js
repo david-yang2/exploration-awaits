dea4dd7a2e9faaa9425a6e424ecbf502c4aadd7d
David <david.yang026@gmail.com>
2021-05-20 15:25:10 -0700

    fetched users

--- frontend/components/carlistings/carlisting_show_container.js
+++ frontend/components/carlistings/carlisting_show_container.js
@@ -2,20 +2,24 @@ import {connect} from "react-redux";
 import CarlistingShow from "./carlisting_show";
 import {fetchCarlisting} from "../../actions/carlistings_actions";
 import {fetchReviews} from "../../actions/reviews_actions"
+import { fetchCurrentUser, fetchAllUsers } from "../../actions/user_actions";
 
 const mapStateToProps = (state, ownProps) => {
-    debugger
     return {
     carlisting: state.carlistings[ownProps.match.params.id],
-    reviews: state.reviews
+    reviews: state.reviews,
+    session: state.session,
+    user: state.user
 }
 }
 
 const mapDispatchToProps = dispatch => {
-    debugger
+
     return{
     fetchCarlisting: id => dispatch(fetchCarlisting(id)),
-    fetchReviews: id => dispatch(fetchReviews(id))
+    fetchReviews: id => dispatch(fetchReviews(id)),
+    fetchAllUsers: () => dispatch(fetchAllUsers()),
+    //fetchCurrentUser: id => dispatch(fetchCurrentUser(id))
     }
 }
 
