fe6b706defff96d8036564d694d30d3d64b4104a
David <david.yang026@gmail.com>
2021-05-21 13:53:40 -0700

    initial commit

---
+++ frontend/components/reviews/create_review.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+class CreateReview extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+
+    render(){
+        return(
+            <h3>Create a review!</h3>
+        )
+    }
+}
+
+export default CreateReview;
