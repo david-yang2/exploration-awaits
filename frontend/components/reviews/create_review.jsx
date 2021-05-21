dd33a5e7903c990c383764cbfcaf4e354f811669
David <david.yang026@gmail.com>
2021-05-21 15:29:18 -0700

    included form to create new reviews

--- frontend/components/reviews/create_review.jsx
+++ frontend/components/reviews/create_review.jsx
@@ -4,12 +4,40 @@ import React from "react"
 class CreateReview extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            carlisting_id: this.props.carlistingId,
+            user_id: this.props.currentUser.id,
+            body: "",
+            rating: null
+        }
     }
 
+    update(field){
+ 
+        return e => {
+    
+          this.setState({[field]: e.currentTarget.value});
+        }
+      }
 
+    handleSubmit(){
+        return this.props.updateReview(this.state)
+
+    }
+    
     render(){
+        debugger
         return(
-            <h3>Create a review!</h3>
+            <div>
+                <h3>Hi {this.props.currentUser.username}, </h3>
+                    <form onSubmit={this.handleSubmit}>
+                    <textarea value={this.state.body}
+                            placeholder="Would you like to create a review?"
+                            onChange = {this.update('body')}
+                    />
+                    <input type="submit" value="Create Review" />
+                </form>
+            </div>
         )
     }
 }
