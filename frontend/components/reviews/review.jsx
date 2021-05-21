74cfb857211887784de9d68af06841c772634b94
David <david.yang026@gmail.com>
2021-05-21 11:25:13 -0700

    handleSubmit and update functions for review

--- frontend/components/reviews/review.jsx
+++ frontend/components/reviews/review.jsx
@@ -4,28 +4,44 @@ import React from "react";
 class Review extends React.Component{
     constructor(props){
         super(props)
+
+        this.state = this.props.review;
+        this.handleSubmit = this.handleSubmit.bind(this);
+        
+    }
+
+    update(field){
+ 
+        return e => {
+    
+          this.setState({[field]: e.currentTarget.value});
+        }
+      }
+
+    handleSubmit(){
+        return this.props.updateReview(this.state)
+
     }
 
     render(){
-
-        // Working code
         return (
             <div>
-                {Object.values(this.props.reviews).map(r => 
-                    Object.values(this.props.users).map(u =>
-                        {if (u["id"] == r.user_id){ return(
-                            <div>
-                                <h3 key={r.id}>Name: {u.username} Review: {r.body}</h3>
-                            </div>
-                        )}
-                        
+                {Object.values(this.props.users).map(user =>
+                    {if (user["id"] == this.props.review.user_id){ return(
+                        <div key={user.id}>
+                            <h5>
+                                Name: {user.username}
+                            </h5>
+                            <form>
+                                Review: {this.props.review.body}
+                            </form>
+                            <br />
+                        </div>
+                                            )}
                     }
-                ))}
+                )}
             </div>
         )
-
-
-        // Piece of shit did not work
     }
 }
 
