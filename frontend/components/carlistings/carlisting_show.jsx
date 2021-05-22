ad80a72ab9e547780eb606d91b1b4f1e888d12a8
David <david.yang026@gmail.com>
2021-05-22 15:42:20 -0700

    imported Calendar component

--- frontend/components/carlistings/carlisting_show.jsx
+++ frontend/components/carlistings/carlisting_show.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Review from "../reviews/review";
 import CreateReview from "../reviews/create_review";
+import Calendar from '../calendar/calendar';
 
 class CarlistingShow extends React.Component{
     constructor(props){
@@ -19,6 +20,7 @@ class CarlistingShow extends React.Component{
     }
 
     createReview(newReview){
+        debugger
         this.props.createRigReview(newReview)
     }
 
@@ -31,15 +33,21 @@ class CarlistingShow extends React.Component{
     render(){
         const {carlisting, reviews, user} = this.props;
         if (!carlisting) return null
-        debugger
         return (Object.keys(reviews).length != undefined && Object.keys(user).length >0 ?
             <div>
                 <div>
                     <h1>{carlisting.year} {carlisting.make} {carlisting.model}</h1>
+                </div>
+                <div>
+                    <Calendar />
+                </div>
+                <div>
                     {this.props.session.currentUser ?     
-                            <CreateReview createReview={this.createReview}
-                                            currentUserId = {this.props.session.currentUser.id}/>
-                            : <h2>Please Login/SignUp to leave a comment</h2>} 
+                            <CreateReview   createReview={this.createReview}
+                                            carlistingId={carlisting.id}
+                                            currentUser = {this.props.session.currentUser}/>
+                            : 
+                            <h2>Please Login/SignUp to leave a comment</h2>} 
                 </div>
                 <div>
                     {Object.values(reviews).map(review => 
