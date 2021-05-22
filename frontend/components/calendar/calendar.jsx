40efb7d34ffbc4944444298491c09ee25190106b
David <david.yang026@gmail.com>
2021-05-22 15:43:58 -0700

    included DateRangePicker

--- frontend/components/calendar/calendar.jsx
+++ frontend/components/calendar/calendar.jsx
@@ -7,9 +7,25 @@ import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'rea
 class Calendar extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            startDate: null,
+            endDate: null
+        }
     }
     render(){
-        return null
+        return (
+            <DateRangePicker
+                startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+                startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+                endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+                endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+                onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+                focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+            />
+
+            
+        )
     }
 }
 
