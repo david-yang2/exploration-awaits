c46064cd3a9caa69166534f2139ccfcc7cff0054
David <david.yang026@gmail.com>
2021-05-22 15:23:40 -0700

    initial commit

---
+++ frontend/components/calendar/calendar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+
+
+class Calendar extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return null
+    }
+}
+
+export default Calendar;
