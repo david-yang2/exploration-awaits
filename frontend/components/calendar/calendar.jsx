9d98368aa2e669a6758f7c22aafb4698c85991d3
David <david.yang026@gmail.com>
2021-05-22 18:13:05 -0700

    Booking button

--- frontend/components/calendar/calendar.jsx
+++ frontend/components/calendar/calendar.jsx
@@ -9,22 +9,41 @@ class Calendar extends React.Component{
         super(props)
         this.state = {
             startDate: null,
-            endDate: null
+            endDate: null,
+            bookStartDate: null,
+            bookEndDate: null,
         }
+        this.bookDates = this.bookDates.bind(this)
     }
-    render(){
-        return (
-            <DateRangePicker
-                startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-                startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-                endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-                endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-                onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
-                focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-                onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-            />
 
-            
+    bookDates(){
+        this.setState({
+            bookStartDate: this.state.startDate._d,
+            endStartDate: this.state.endDate._d
+        })
+        console.log(this.state)
+    }
+
+    render(){
+        debugger
+        return (
+            <div>
+                <div>
+                    <DateRangePicker
+                        startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+                        startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+                        endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+                        endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+                        onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+                        focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+                        onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+                    />
+                    
+                </div>
+                <button onClick={ () => this.bookDates()}> Book now! </button>
+            </div>
+
+
         )
     }
 }
