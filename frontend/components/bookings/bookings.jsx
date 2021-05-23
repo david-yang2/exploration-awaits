83bd5b6a5c77bc0d3eadb95637dbe061a18dd733
David <david.yang026@gmail.com>
2021-05-23 14:15:28 -0700

    initial commit

---
+++ frontend/components/bookings/bookings.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+class Bookings extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return null
+    }
+}
+
+export default Bookings
