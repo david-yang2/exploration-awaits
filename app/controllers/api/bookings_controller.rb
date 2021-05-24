f377e473ad553a0037932ef2d919899e6b565369
David <david.yang026@gmail.com>
2021-05-23 22:12:07 -0700

    fixed rendering for create action

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,35 +1,36 @@
 class Api::BookingsController < ApplicationController
 
-    #GET    /api/bookings
+    #GET    /api/users/:user_id/bookings
     def index
-        @bookings = Booking.all
+        @user = User.find(params[:user_id])
+        render json: @user.bookings
     end
 
-    #GET    /api/bookings/:id
-    def show
-        @booking = Booking.find(params[:id])
-    end
+    # #GET    /api/bookings/:id
+    # def show
+    #     @booking = Booking.find(params[:id])
+    # end
 
-    #POST   /api/bookings
+    #POST   /api/users/:user_id/bookings
     def create
         @booking = Booking.new(booking_params)
         if @booking.save
-            return
+            render :show
         else
             render json: @booking.errors.full_messages, status: 422
         end
     end
 
-    #PATCH  /api/bookings/:id
-    def update
-        @booking = Booking.find(params[:id])
+    # #PATCH  /api/bookings/:id
+    # def update
+    #     @booking = Booking.find(params[:id])
 
-        if @booking.update(booking_params)
-            return
-        else 
-            render json: @booking.errors.full_messages, status: 422
-        end
-    end
+    #     if @booking.update(booking_params)
+    #         return
+    #     else 
+    #         render json: @booking.errors.full_messages, status: 422
+    #     end
+    # end
 
     #DELETE /api/bookings/:id
     def destroy
