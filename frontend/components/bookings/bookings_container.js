3508e2cb90b449f83a128a6c18db56fc87a1d021
David <david.yang026@gmail.com>
2021-05-25 11:52:35 -0700

    connected bookings presentational

---
+++ frontend/components/bookings/bookings_container.js
@@ -0,0 +1,12 @@
+import connect from "react-redux"
+import Bookings from "./bookings"
+
+const mapStateToProps = dispatch => {
+    return null
+}
+
+const mapDispatchToProps = dispatch => {
+    return null
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Bookings)
