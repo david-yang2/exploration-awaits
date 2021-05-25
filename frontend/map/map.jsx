a904f3f712766641a3b8dec86e23fba422524eee
David <david.yang026@gmail.com>
2021-05-25 10:34:48 -0700

    initial commit

---
+++ frontend/map/map.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {GoogleMaps} from "react-google-maps";
+
+
+
+class Map extends React.Component{
+    constructor(props){
+        super(props)
+
+    }
+
+
+
+
+
+    render(){
+        return null
+    }
+}
+
+
+
+export default Map;
