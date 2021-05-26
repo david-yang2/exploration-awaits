17503c0afeb9e30f5405179964533f11f2f84f72
David <david.yang026@gmail.com>
2021-05-26 16:57:05 -0700

    rendered bookings pick up and drop off date

--- frontend/components/bookings/bookings.jsx
+++ frontend/components/bookings/bookings.jsx
@@ -6,8 +6,28 @@ class Bookings extends React.Component{
         super(props)
     }
 
+    componentDidMount(){
+        this.props.fetchBookings(this.props.session.currentUser.id)
+
+    }
+
+
+
+
+
     render(){
-        return null
+        const {bookings} = this.props
+        debugger
+        if (!bookings) return null
+        return(
+            <div>
+                {Object.values(bookings).map(booking=>
+                                        <h5 key={booking.id}>
+                                            {booking.pickup_date} {booking.dropoff_date}
+                                        </h5>
+                )}
+            </div>
+        )
     }
 }
 
