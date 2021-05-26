21f05b6c434b2a0187b18246104cdd519dcf6b91
David <david.yang026@gmail.com>
2021-05-26 13:27:44 -0700

    saved selected filter options

--- frontend/components/filter/filter.jsx
+++ frontend/components/filter/filter.jsx
@@ -4,19 +4,31 @@ class Filter extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            listingKeys: Object.keys(this.props.carlistings[0]),
             make: [],
             model: [],
             drivetrain: [],
+            sleeps: [],
+            selectedMake: [],
+            selectedModel: [],
+            selectedDrivetrain: [],
+            selectedSleeps: []
         }
 
     this.getUniqueValues = this.getUniqueValues.bind(this)
     this.filterListings = this.filterListings.bind(this)
-    this.handleToggle = this.handleToggle.bind(this)
+    this.handleSelect = this.handleSelect.bind(this)
+    // this.filterCheckBox = this.filterCheckBox.bind(this)
     }
 
-    handleToggle(){
-        return null
+    handleSelect(key, value){
+        debugger
+        return e => {
+            // this.setState({[key]: e.currentTarget.value});
+            this.setState(prevState => ({
+                [key]: [...prevState[key], value]
+              }))
+            debugger
+        }
     }
     getUniqueValues(key){
         const data = this.props.carlistings
@@ -27,70 +39,36 @@ class Filter extends React.Component{
     filterListings(key){
         const unique = this.getUniqueValues(key)
         this.setState({[key]: unique})
-        debugger
     }
+
     render(){
+        const selected = ["make", "model", "drivetrain"]
+        const {selectedMake, selectedModel, selectedDrivetrain} = this.state
         return (
             <div>
-                {/* Make button and checkbox */}
-                <div>
-                    <button onClick={() => this.filterListings("make")}>Make</button>
+                <form className="show-filters">
+                    {/* Make button and checkbox */}
                     <div>
-                        {Object.values(this.state.make).map(value => (
-                            <div key={value}> 
-                                <input
-                                type="checkbox"
-                                onChange={this.handleToggle}
-                                id={value}
-                                />
-                                <label for={value}>
-                                    {value}
-                                </label>
-                            </div>
-                        ))}
+                        <button onClick={() => this.filterListings("make")}>Make</button>
+                        <div>
+                            {Object.values(this.state.make).map(value => (
+                                <div key={value}> 
+                                    <input
+                                    type="checkbox"
+                                    onChange={this.handleSelect("selectedMake", value)}
+                                    id={value}
+                                    />
+                                    <label for={value}>
+                                        {value}
+                                    </label>
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                </div>
-                {/* Model button and checkbox */}
-                <div>
-                    <button onClick={() => this.filterListings("model")}>Model</button>
-                    <div>
-                        {Object.values(this.state.model).map(value => (
-                            <div key={value}> 
-                                <input
-                                type="checkbox"
-                                onChange={this.handleToggle}
-                                id={value}
-                                />
-                                <label for={value}>
-                                    {value}
-                                </label>
-                            </div>
-                        ))}
-                    </div>
-
-                </div>
-                {/* Drivetrain button and checkbox */}
-                <div>
-                    <button onClick={() => this.filterListings("drivetrain")}>Drivetrain</button>
-                    <div>
-                        {Object.values(this.state.drivetrain).map(value => (
-                            <div key={value}> 
-                                <input
-                                type="checkbox"
-                                onChange={this.handleToggle}
-                                id={value}
-                                />
-                                <label for={value}>
-                                    {value}
-                                </label>
-                            </div>
-                        ))}
-                    </div>
-
-                </div>
-
-
-                
+            
+                    <input type="submit" value="Filter" />
+                </form>
+                <h2>{selectedMake}</h2>
             </div>
         )
     }
@@ -98,3 +76,74 @@ class Filter extends React.Component{
 }
 
 export default Filter;
+
+
+
+
+
+    //     {/* Model button and checkbox */}
+    //     <div>
+    //     <button onClick={() => this.filterListings("model")}>Model</button>
+    //     <div>
+    //         {Object.values(this.state.model).map(value => (
+    //             <div key={value}> 
+    //                 <input
+    //                 type="checkbox"
+    //                 onChange={this.handleSelect("slectedModel", value)}
+    //                 id={value}
+    //                 />
+    //                 <label for={value}>
+    //                     {value}
+    //                 </label>
+    //             </div>
+    //         ))}
+    //     </div>
+
+    // </div>
+    // {/* Drivetrain button and checkbox */}
+    // <div>
+    //     <button onClick={() => this.filterListings("drivetrain")}>Drivetrain</button>
+    //     <div>
+    //         {Object.values(this.state.drivetrain).map(value => (
+    //             <div key={value}> 
+    //                 <input
+    //                 type="checkbox"
+    //                 onChange={this.handleSelect("selectedDrivetrain", value)}
+    //                 id={value}
+    //                 />
+    //                 <label for={value}>
+    //                     {value}
+    //                 </label>
+    //             </div>
+    //         ))}
+    //     </div>
+
+    // </div>
+    // {/* Sleeps button and range selector */}
+    // <div>
+    //     <button onClick={() => this.filterListings("sleeps")}>Sleeps</button>
+    //     <div>
+    //         {Object.values(this.state.sleeps).map(value => (
+    //             <div key={value}> 
+    //                 <input
+    //                 type="range"
+    //                 onChange={this.handleSelect("selectedSleeps", value)}
+    //                 id={value}
+    //                 min="0"
+    //                 max="5"
+    //                 list="tickmarks"/>
+    //                 <datalist id='tickmarks'>
+    //                     <option value='0' label='0'></option>
+    //                     <option value='1' label='1'></option>
+    //                     <option value='2' label='2'></option>
+    //                     <option value='3' label='3'></option>
+    //                     <option value='4' label='4'></option>
+    //                     <option value='5' label='5'></option>
+    //                 </datalist>
+                    
+    //             </div>
+    //         ))}
+    //     </div>
+
+    // </div>
+    
