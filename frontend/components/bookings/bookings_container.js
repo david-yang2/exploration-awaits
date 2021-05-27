b5038de7435a2d2e1b6f15ad8c7eaaa5ac3fe9eb
David <david.yang026@gmail.com>
2021-05-26 23:23:10 -0700

    fetchCarlisting

--- frontend/components/bookings/bookings_container.js
+++ frontend/components/bookings/bookings_container.js
@@ -1,12 +1,25 @@
-import connect from "react-redux"
+import {connect} from "react-redux"
 import Bookings from "./bookings"
+import {fetchBookings, removeBooking} from "../../actions/bookings_actions"
+import { fetchCarlistings } from "../../actions/carlistings_actions"
 
-const mapStateToProps = dispatch => {
-    return null
-}
+const mapStateToProps = (state, ownProps) => {
+    debugger
+    return{
+    bookings: state.bookings,
+    session: state.session,
+    user: state.user,
+    carlistings: state.carlistings
+}}
 
 const mapDispatchToProps = dispatch => {
-    return null
+    return{
+    fetchBookings: id => dispatch(fetchBookings(id)),
+    removeBooking: booking => dispatch(removeBooking(booking)),
+    fetchCarlistings: () => dispatch(fetchCarlistings())
+    }
+    
+    
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Bookings)
