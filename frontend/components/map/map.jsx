b4e2fd4f312446d8eb106860b9ecb18e488fab1c
David <david.yang026@gmail.com>
2021-05-27 17:39:03 -0700

    functional pt A to B rendering

--- frontend/components/map/map.jsx
+++ frontend/components/map/map.jsx
@@ -1,22 +1,34 @@
 import React from "react";
+import MarkerManager from "../../util/marker_manager"
 
 
 class Map extends React.Component{
     constructor(props){
         super(props)
-
     }
 
     componentDidMount(){
 
-        // set the map to show SF
-        const mapOptions = {
-        center: { lat: 37.7758, lng: -122.435 }, // this is SF
-        zoom: 13
-        };
+      // set the map to show SF
+      const mapOptions = {
+      center: { lat: 37.7758, lng: -122.435 }, // this is SF
+      zoom: 12
+      };
 
-        // wrap this.mapNode in a Google Map
-        this.map = new google.maps.Map(this.mapNode, mapOptions);
+      // wrap this.mapNode in a Google Map
+      this.map = new google.maps.Map(this.mapNode, mapOptions);
+
+      //connect to MarkerManager to interact with it
+      // create new MarkerManager
+      this.MarkerManager = new MarkerManager(this.map)
+      this.MarkerManager.updateMarkers()
+      this.MarkerManager.calcRoute()
+
+
+    }
+
+    componentDidUpdate(){
+      this.MarkerManager.updateMarkers()
     }
 
     render() {
