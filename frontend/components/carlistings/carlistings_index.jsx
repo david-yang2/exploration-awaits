14eb069a12cd06d368af89b5c56baec767d8df37
David <david.yang026@gmail.com>
2021-05-28 11:35:12 -0700

    created selections for map centering

--- frontend/components/carlistings/carlistings_index.jsx
+++ frontend/components/carlistings/carlistings_index.jsx
@@ -11,7 +11,8 @@ class CarlistingsIndex extends React.Component{
               center: {lat:38.57283716077039, lng:-109.55545695813444}, // Moab, UT
             //   center: {lat:37.84755373063942, lng:-119.18447474550449}, // Yosemite Ntnl Park
               zoom: 12
-            }
+            },
+            defaultLoc: "SF"
           }
         this.selectCenter = this.selectCenter.bind(this)
     }
@@ -20,16 +21,17 @@ class CarlistingsIndex extends React.Component{
         this.props.fetchCarlistings();
     }
 
+
+    // Options for center Google Maps
     selectCenter(value){
         var mapOptions = {...this.state.mapOptions}
-        
-        if (value === "San Francisco, CA"){
+        if (value === "SF"){
             mapOptions.center = {lat: 37.7758, lng: -122.435 }
             this.setState({ mapOptions})}
-        else if (value === "Moab, UT") {
+        else if (value === "MOAB") {
             mapOptions.center = {lat:38.57283716077039, lng:-109.55545695813444}
             this.setState({ mapOptions})}
-        else if (value === "Yosemite National Park, CA") {
+        else if (value === "Yosemite") {
             mapOptions.center = {lat:37.84755373063942, lng:-119.18447474550449}
             this.setState({ mapOptions})}
         }
@@ -40,13 +42,15 @@ class CarlistingsIndex extends React.Component{
         if (!carlistings || carlistings.length<1) return null;
         return(
             <div>
-                <form>
-
-                </form>
                 <div>
                     <Filter carlistings={carlistings} />
                 </div>
                 <div>
+                    <select value={this.state.defaultLoc} onChange={this.selectCenter(value)}>
+                        <option value="SF">San Francisco, CA</option>
+                        <option value="MOAB">Moab, UT</option>
+                        <option value="Yosemite">Yosemite National Park, CA</option>
+                    </select>
                     <Map carlistings={carlistings}
                             mapOptions={this.state.mapOptions}/>
                 </div>
