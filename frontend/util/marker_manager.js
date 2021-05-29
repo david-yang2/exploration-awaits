1321584bb6c94abb826e175e45c3328838eb0c0b
David <david.yang026@gmail.com>
2021-05-28 17:00:26 -0700

    included infowindow for each marker

--- frontend/util/marker_manager.js
+++ frontend/util/marker_manager.js
@@ -3,16 +3,16 @@ export default class MarkerManager {
     constructor(map){
         this.map = map;
         this.markers=[];
+
+        this.directionsService = new google.maps.DirectionsService();
+        this.directionsRenderer = new google.maps.DirectionsRenderer();
+        this.directionsRenderer.setMap(map);
+       
     }
 
-    addMarker() {
-        this.map.addListener("click", (e) =>{
-            this.staticMarker(e.latLng)
-        })
-    }
 
-
-    staticMarker(location){
+    staticMarker(location, content){
+        debugger
         // creates a marker on the map
         const marker = new google.maps.Marker({
             // position: {lat: latitude, lng: longitude},
@@ -20,25 +20,18 @@ export default class MarkerManager {
             map: this.map
         })
         this.markers.push(marker)
-    }
-    //<button onClick={()=>this.MarkerManager.undo()}>Delete</button>
+        const infowindow = new google.maps.InfoWindow({
+            content: content,
+          });
 
-    // setMapOnAll(map){
-    //     for (let i= 0; i < this.markers.length; i++){
+        marker.addListener("click", () => {
+            infowindow.open(this.map, marker);
+        });
 
-    //         //setMap is a method of google.maps
-    //         this.markers[i].setMap(map)
-    //     }
-    // }
-    
-    undo(){
-        debugger
-        console.log("asdf")
-        this.markers[this.markers.length - 1].setMap(null)
-        this.markers = this.markers.slice(0,-1)
+
     }
 
-
+    // sample trip from San Francisco to Zion National Park
     displayRoute() {
         var directionsService = new google.maps.DirectionsService();
         var directionsRenderer = new google.maps.DirectionsRenderer();
@@ -57,7 +50,121 @@ export default class MarkerManager {
             }
         });
     }
-    
+
+
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // undo(){
+    //     this.markers[this.markers.length - 1].setMap(null)
+    //     this.markers = this.markers.slice(0,-1)
+    //     console.log("deleted previous marker")
+    // }
+    
+
   
+    //<button onClick={()=>this.MarkerManager.undo()}>Delete</button>
+
+    // setMapOnAll(map){
+    //     for (let i= 0; i < this.markers.length; i++){
+
+    //         //setMap is a method of google.maps
+    //         this.markers[i].setMap(map)
+    //     }
+    // }
+
+
+
+// // dynamic markers
+//     addMarker(){
+//         this.map.addListener("click", (e) => {
+//             this.staticMarker(e.latLng)
+//             this.calculateAndDisplayRoute(this.directionsService, this.directionsRenderer)
+    
+//         })
+//         };   
+    
+//         undo(){
+//             // this.markers[this.markers.length - 1].setMap(null)
+//             this.markers = this.markers.slice(0,-1)
+//             console.log("deleted previous marker")
+//             // this.directionsRenderer.setMap(this.map)
+//         }
+    
+//         staticMarker(location){
+//             // creates a marker on the map
+//             const marker = new google.maps.Marker({
+//                 // position: {lat: latitude, lng: longitude},
+//                 position: location,
+//                 map: this.map
+//             })
+//             this.markers.push({location: {lat: marker.position.lat(), lng: marker.position.lng()}, stopover: true})
+//         }
+    
+//         calculateAndDisplayRoute(directionsService, directionsRenderer) {
+    
+//         if(this.markers.length <2) return null
+        
+//         // define the waypts
+//         // if this.markers has 2 elements in there, there are no "waypoints"
+//         // if this.markers has more than 2 elements in there, waypoints are elements inbetween the first and last
+//         let waypts = []
+//         if (this.markers.length>2){
+//             waypts = this.markers.slice(1,-1)
+//         }
+    
+//         directionsService.route(
+//             {
+//             origin: this.markers[0].location,
+//             destination: this.markers[this.markers.length-1].location,
+//             waypoints: waypts,
+//             optimizeWaypoints: true,
+//             travelMode: google.maps.TravelMode.DRIVING,
+//             },
+//             (response, status) => {
+//             if (status === "OK" && response) {
+//                 directionsRenderer.setDirections(response);
+//                 const route = response.routes[0];
+//                 const summaryPanel = document.getElementById("directions-panel");
+//                 summaryPanel.innerHTML = "";
+    
+//                 // For each route, display summary information.
+//                 for (let i = 0; i < route.legs.length; i++) {
+//                 const routeSegment = i + 1;
+//                 summaryPanel.innerHTML +=
+//                     "<b>Route Segment: " + routeSegment + "</b><br>";
+//                 summaryPanel.innerHTML += route.legs[i].start_address + " to ";
+//                 summaryPanel.innerHTML += route.legs[i].end_address + "<br>";
+//                 summaryPanel.innerHTML += route.legs[i].distance.text + "<br><br>";
+//                 }
+//             } else {
+//                 window.alert("Directions request failed due to " + status);
+//             }
+//             }
+//         );
