dcb9d10331100a0007691b1f8c02f95790e34b5f
David <david.yang026@gmail.com>
2021-06-01 14:33:28 -0700

    updated user routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,15 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
 
     resources :carlistings, except: [:new, :edit] do
-      resources :reviews, except: [:new, :edit, :show]
+      resources :reviews, except: [:new, :edit, :show, :update]
       resources :trips, only: [:show]
     end
     resources :trips, only: [:index, :create]
-    resources :bookings, except: [:new, :edit]
-    resources :users, except: [:new, :edit]
+    resources :reviews, only: [:update, :show]
+    resources :bookings, only: [:destroy]
+    resources :users, except: [:new, :edit] do 
+      resources :bookings, only: [:index, :create]
+    end
     resource :session, only: [:new, :create, :destroy]
   end
   
