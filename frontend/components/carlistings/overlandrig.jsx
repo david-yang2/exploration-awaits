69bd02a3838358371cb5eb7a3a108028ee172733
David <david.yang026@gmail.com>
2021-05-31 23:02:50 -0700

    updated filter for car models

--- frontend/components/carlistings/overlandrig.jsx
+++ frontend/components/carlistings/overlandrig.jsx
@@ -5,6 +5,12 @@ const OverlandRig = (props) => {
     return (
         <div>
             <Link to={`/carlistings/${props.rig.id}`}>{props.rig.year} {props.rig.make} {props.rig.model}</Link>
+            <h4>Features:</h4>
+            <body>Cartype: {props.rig.cartype},
+                  Drivetrain: {props.rig.drivetrain},
+                  Transmission: {props.rig.transmission},
+                  Lockers: {props.rig.lockers}
+            </body>
             <h5>$ {props.rig.price} / day</h5>
             <br />
             
