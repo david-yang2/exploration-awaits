6e57470d64eb73a03c7b4679c839482bbf621960
David <david.yang026@gmail.com>
2021-06-01 22:03:19 -0700

    updated foreign_key

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
     belongs_to :carlisting,
         primary_key: :id,
-        foreign_key: :carlisting_id,
+        foreign_key: :car_id,
         class_name: :Carlisting
 
 end
