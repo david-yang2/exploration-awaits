83f9ccc9c04b6e3088535ff5892763da3c482d21
David <david.yang026@gmail.com>
2021-06-01 22:10:40 -0700

    included image for listings

--- frontend/components/carlistings/overlandrig.jsx
+++ frontend/components/carlistings/overlandrig.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 
+
 const OverlandRig = (props) => {
+    const id = props.photoidx
     return (
         <div>
-            <Link to={`/carlistings/${props.rig.id}`}>{props.rig.year} {props.rig.make} {props.rig.model}</Link>
-            <h4>Features:</h4>
-            <body>Cartype: {props.rig.cartype},
-                  Drivetrain: {props.rig.drivetrain},
-                  Transmission: {props.rig.transmission},
-                  Lockers: {props.rig.lockers}
-            </body>
+            <img src={`rigs/carlisting${id}/image1.jpeg`} width="250px" height ="200px"alt='' />
+            <br/>
+            <Link to={{
+                pathname:`/carlistings/${props.rig.id}`,
+                state:  { photoidx: props.photoidx}}
+            } > {props.rig.year} {props.rig.make} {props.rig.model}</Link>
+                
+                
+            <h4>Features:
+                <body>Cartype: {props.rig.cartype},
+                    Drivetrain: {props.rig.drivetrain},
+                    Transmission: {props.rig.transmission},
+                    Lockers: {props.rig.lockers}
+                </body>
+            </h4>
             <h5>$ {props.rig.price} / day</h5>
             <br />
             
