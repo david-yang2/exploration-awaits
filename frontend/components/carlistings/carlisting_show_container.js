a291352881fc3552d5dbf9df29792b747481378c
David <david.yang026@gmail.com>
2021-06-02 14:48:12 -0700

    included fetchTrips dispatch and trips state

--- frontend/components/carlistings/carlisting_show_container.js
+++ frontend/components/carlistings/carlisting_show_container.js
@@ -5,14 +5,16 @@ import {fetchAllUsers} from "../../actions/user_actions";
 import {fetchReviews,
         createRigReview,
         updateRigReview} from "../../actions/reviews_actions";
-import {createNewBooking} from "../../actions/bookings_actions"
+import {createNewBooking} from "../../actions/bookings_actions";
+import {fetchTrips} from "../../actions/trips_actions"
 
 const mapStateToProps = (state, ownProps) => {
     return {
     carlisting: state.carlistings[ownProps.match.params.id],
     reviews: state.reviews,
     session: state.session,
-    user: state.user
+    user: state.user,
+    trips: state.trips
     }
 }
 
@@ -24,6 +26,8 @@ const mapDispatchToProps = dispatch => {
     createRigReview: newReview => dispatch(createRigReview(newReview)),
     updateRigReview: review => dispatch(updateRigReview(review)),
     createNewBooking: booking => dispatch(createNewBooking(booking)),
+    fetchTrips: carlisting_id => dispatch(fetchTrips(carlisting_id))
+
     }
 }
 
