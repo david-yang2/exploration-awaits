94b2ce3f1f151b541ca2d8523c53e15329db1b7d
David <david.yang026@gmail.com>
2021-06-02 13:25:12 -0700

    changed name for request

--- frontend/util/trips_api_util.js
+++ frontend/util/trips_api_util.js
@@ -6,7 +6,7 @@
 // get trips for specific carlisting
 //GET    /api/carlistings/:carlisting_id/trips
 
-export const fetchTrips = carlisting_id => {
+export const getTrips = carlisting_id => {
     return $.ajax({
         url: `/api/carlistings/${carlisting_id}/trips`,
         method: "GET"
