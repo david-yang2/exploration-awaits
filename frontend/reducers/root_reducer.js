5f1009cb21f9a967f4cea1ccb4eabf99f7163b8a
David <david.yang026@gmail.com>
2021-06-02 13:30:06 -0700

    included TripsReducer: :

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -4,13 +4,15 @@ import CarlistingsReducer from "./carlistings_reducer"
 import ReviewsReducer from "./reviews_reducer"
 import UsersReducer from "./user_reducer"
 import BookingsReducer from "./bookings_reducer"
+import TripsReducer from "./trips_reducer"
 
 const RootReudcer = combineReducers({
     carlistings: CarlistingsReducer,
     session: SessionReducer,
     reviews: ReviewsReducer,
     user: UsersReducer,
-    bookings: BookingsReducer
+    bookings: BookingsReducer,
+    trips: TripsReducer,
 
 });
 
