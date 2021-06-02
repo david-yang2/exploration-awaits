b0c1ef471fe8e77462eaade21da88d2605cdcf45
David <david.yang026@gmail.com>
2021-06-02 15:34:15 -0700

    included imageIdx for images reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_213217) do
+ActiveRecord::Schema.define(version: 2021_06_02_221955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_05_06_213217) do
     t.float "longitude", null: false
     t.float "latitude", null: false
     t.string "location", null: false
+    t.integer "imageIdx", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
