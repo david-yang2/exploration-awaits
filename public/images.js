3d0123ac506090301292214ba1cdf6fd001df6da
David <david.yang026@gmail.com>
2021-06-04 20:30:35 -0700

    image paths for carlisting_show component

---
+++ public/images.js
@@ -0,0 +1,68 @@
+export const carlisting0 = [
+    `rigs/carlisting0/image1.jpeg`,
+    `rigs/carlisting0/image2.jpeg`,
+    `rigs/carlisting0/image3.jpeg`,
+    `rigs/carlisting0/image4.jpeg`,
+]
+
+export const carlisting1 = [
+    `rigs/carlisting1/image1.jpeg`,
+    `rigs/carlisting1/image2.jpeg`,
+    `rigs/carlisting1/image3.jpeg`,
+    `rigs/carlisting1/image4.jpeg`,
+]
+
+export const carlisting2 = [
+    `rigs/carlisting2/image1.jpeg`,
+    `rigs/carlisting2/image2.jpeg`,
+    `rigs/carlisting2/image3.jpeg`,
+]
+
+export const carlisting3 = [
+    `rigs/carlisting3/image1.jpeg`,
+    `rigs/carlisting3/image2.jpeg`,
+    `rigs/carlisting3/image3.jpeg`,
+    `rigs/carlisting3/image4.jpeg`,
+]
+
+export const carlisting4 = [
+    `rigs/carlisting4/image1.jpeg`,
+    `rigs/carlisting4/image2.jpeg`,
+    `rigs/carlisting4/image3.jpeg`,
+]
+
+export const carlisting5 = [
+    `rigs/carlisting5/image1.jpeg`,
+    `rigs/carlisting5/image2.jpeg`,
+    `rigs/carlisting5/image3.jpeg`,
+
+]
+
+export const carlisting6 = [
+    `rigs/carlisting6/image1.jpeg`,
+    `rigs/carlisting6/image2.jpeg`,
+    `rigs/carlisting6/image3.jpeg`,
+    `rigs/carlisting6/image4.jpeg`,
+]
+
+export const carlisting7 = [
+    `rigs/carlisting7/image1.jpeg`,
+    `rigs/carlisting7/image2.jpeg`,
+    `rigs/carlisting7/image3.jpeg`,
+    `rigs/carlisting7/image4.jpeg`,
+]
+
+export const carlisting8 = [
+    `rigs/carlisting8/image1.jpeg`,
+    `rigs/carlisting8/image2.jpeg`,
+    `rigs/carlisting8/image3.jpeg`,
+    `rigs/carlisting8/image4.jpeg`,
+]
+
+
+export const carlisting9 = [
+    `rigs/carlisting9/image1.jpeg`,
+    `rigs/carlisting9/image2.jpeg`,
+    `rigs/carlisting9/image3.jpeg`,
+    `rigs/carlisting9/image4.jpeg`,
+]
