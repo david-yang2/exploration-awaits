63dc2f08a99c0323688f9c65099a5abd9d2887d9
David <david.yang026@gmail.com>
2021-06-16 22:27:41 -0700

    clickable image for each carlisting

--- frontend/components/carlistings/overlandrig.jsx
+++ frontend/components/carlistings/overlandrig.jsx
@@ -5,23 +5,25 @@ import {Link} from 'react-router-dom';
 const OverlandRig = (props) => {
     const id = props.rig.imageIdx
     return (
-        <div>
-            <img src={`rigs/carlisting${id}/image1.jpeg`} width="250px" height ="200px"alt='' />
+        <div className="rigContainer">
+            {/* link to show page when image is clicked */}
+            <a href={`#/carlistings/${props.rig.id}`}>
+                <img className="imgidx" src={`rigs/carlisting${id}/image1.jpeg`} width="250px" height ="200px"alt='' />
+            </a>
             <br/>
-            <Link to={{
-                pathname:`/carlistings/${props.rig.id}`,
-                state:  { imageIdx: id}}
-            } > {props.rig.year} {props.rig.make} {props.rig.model}</Link>
+            <div className="rig-title">
+                <div>{props.rig.year} {props.rig.make} {props.rig.model}</div>
+                <h5>${props.rig.price}/day </h5>
+            </div>
                 
-                
-            <h4>Features:
-                <body>Cartype: {props.rig.cartype},
+            <h5>Features:
+                <body className='rig-features'>Cartype: {props.rig.cartype},
                     Drivetrain: {props.rig.drivetrain},
                     Transmission: {props.rig.transmission},
                     Lockers: {props.rig.lockers}
                 </body>
-            </h4>
-            <h5>$ {props.rig.price} / day</h5>
+            </h5>
+            
             <br />
             
         </div>
@@ -30,3 +32,10 @@ const OverlandRig = (props) => {
 };
 
 export default OverlandRig;
+
+
+
+// <Link to={{
+//     pathname:`/carlistings/${props.rig.id}`,
+//     state:  { imageIdx: id}}
+// } >{props.rig.year} {props.rig.make} {props.rig.model}</Link>
