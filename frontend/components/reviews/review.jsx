66ea2b72e39cc2435774499d30723d878c512398
David <david.yang026@gmail.com>
2021-10-05 13:05:30 -0700

    review modal

--- frontend/components/reviews/review.jsx
+++ frontend/components/reviews/review.jsx
@@ -1,61 +1,78 @@
 import React from "react";
+import Avatar from "../avatar/avatar"
 
 
 class Review extends React.Component{
     constructor(props){
         super(props)
 
-        this.state = this.props.review;
+        this.state = {
+            review: this.props.review,
+            edit: false
+        }
         this.handleSubmit = this.handleSubmit.bind(this);
-        
+        this.toggleEdit = this.toggleEdit.bind(this)
     }
 
     update(field){
         return e => {
-    
-          this.setState({[field]: e.currentTarget.value});
+          this.setState({review:{...this.state.review, [field]: e.currentTarget.value}});
         }
       }
 
     handleSubmit(){
-        return this.props.updateReview(this.state)
+        this.props.updateReview(this.state.review)
 
+        //close chat modal
+        this.toggleEdit()
+    }
+
+    toggleEdit(){
+        this.setState({edit: !this.state.edit})
     }
 
     render(){
-        const form =   this.props.currentUserId === this.props.review.user_id ? 
-            (<div><form onSubmit={this.handleSubmit}>
-                <textarea value={this.state.body}
-                        onChange = {this.update('body')}
-                />
-                <input type="submit" value="Update Review" />
-            </form>
-            <br />
-            </div>) 
-            :
-            (<textarea value={this.state.body} />)
+
+
+        // edit chat modal
+        let editDisplay;
+
+        if (this.state.edit === false){
+            editDisplay = (<div></div>)
+        } else {
+            editDisplay = (
+                <div className="editReviewModal">
+                    <button onClick={()=>this.toggleEdit()}>Close</button>
+                    <form onSubmit={this.handleSubmit}>
+                        <textarea value={this.state.review.body}
+                                onChange = {this.update('body')}
+                        />
+                        <input type="submit" value="Update Previous Review" />
+                    </form>
+                </div>
+            )
+        }
+
         return (
             // list all reviews associated with carlisting and name of users who created the review
             <div className="prvsReviewsSubContnr">
                 {Object.values(this.props.users).map(user =>
                     {if (user["id"] == this.props.review.user_id){ return(
                         <div className="prevReviews" key={user.id}>
+                            <div className='reviewUserInfo'>
+                                <Avatar username={user.username} />
+                                <div style={{marginTop:"10px"}}>{user.username}</div>
+                            </div>
+                            <div className='userReview'>
+                                <text>{this.state.review.body}</text>
 
-
-                            {/* can only update review belonging to current user */}
+                                {/* can only update review belonging to current user */}
                                 {this.props.currentUserId === this.props.review.user_id ? 
-                                (
-                                <form className="updateForm" onSubmit={this.handleSubmit}>
-                                    <textarea className="currentUsersReview" value={this.state.body}
-                                            onChange = {this.update('body')}
-                                    />
-                                    <input className="updateReviewBtn" type="submit" value="Update Previous Review" />
-                                </form>
-                                ) 
+                                <button className='editReview'onClick={()=> this.toggleEdit()}> Edit</button> 
                                 :
-                                (
-                                <text className="othersReviews">{user.username}: "{this.state.body}"</text>
-                                )}
+                                null}
+                            </div>
+                            {editDisplay}
                         </div>
                                             )}
                     }
@@ -66,3 +83,54 @@ class Review extends React.Component{
 }
 
 export default Review;        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form =   this.props.currentUserId === this.props.review.user_id ? 
+// (<div>
+    
+//     <form onSubmit={this.handleSubmit}>
+//     <textarea value={this.state.body}
+//             onChange = {this.update('body')}
+//     />
+//     <input type="submit" value="Update Review" />
+// </form>
+// <br />
+// </div>) 
+// :
+// (<textarea value={this.state.body} />)
+
