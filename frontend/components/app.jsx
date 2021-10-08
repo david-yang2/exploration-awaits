8be794c90b90007b406e8302d01078ba3feac5b0
David <david.yang026@gmail.com>
2021-10-07 21:22:04 -0700

    AuthRoute and ProtectedRoutes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -7,16 +7,17 @@ import LoginContainer from "./login/login_container";
 import CarlistingsIndexContainer from "./carlistings/carlistings_index_container";
 import CarlistingShowContainer from "./carlistings/carlisting_show_container";
 import BookingsContainer from "./bookings/bookings_container"
+import {AuthRoute, ProtectedRoute} from "../util/route_api_util"
 
 const App = () => (
-    <div>
+    <div className='app'>
         <Route path="/" component={NavBarContainer} />
         <Route exact path="/" component ={Home} />
         <Route path="/rigs" component ={CarlistingsIndexContainer} />
-        <Route path="/signup" component={SignupContainer} />
-        <Route path="/login" component={LoginContainer} />
+        <AuthRoute path="/signup" component={SignupContainer} />
+        <AuthRoute path="/login" component={LoginContainer} />
         <Route exact path="/carlistings/:id" component={CarlistingShowContainer} />
-        <Route exact path="/bookings/:id" component={BookingsContainer} />
+        <ProtectedRoute exact path="/bookings/:id" component={BookingsContainer} />
     </div>
 )
 
