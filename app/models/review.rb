09d1845cf16d103539afcf1b6ab76c0c2f9f9adc
David <david.yang026@gmail.com>
2021-11-05 12:37:52 -0700

    validate review format

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,14 @@
 class Review < ApplicationRecord
+    validates: review_format
+
+    def review_format
+        if body.length < 5
+            errors[:body] << "too short"
+        elsif body.length > 600
+            errors[:body] << "too long"
+        end
+    end
+
     belongs_to :user,
         primary_key: :id,
         foreign_key: :user_id,
